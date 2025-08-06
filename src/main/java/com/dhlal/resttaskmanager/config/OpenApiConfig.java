df3aae06ccd5913f81af4136a0b517d60055badf
HLAL <62333848+HlalDriss@users.noreply.github.com>
2025-08-06 20:27:22 +0000

    change props

--- src/main/java/com/dhlal/resttaskmanager/config/OpenApiConfig.java
+++ src/main/java/com/dhlal/resttaskmanager/config/OpenApiConfig.java
@@ -7,14 +7,19 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${openapi.server-url:/}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI taskApiDoc() {
         return new OpenAPI()
-                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url("/"))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url(serverUrl))
                 .info(new Info()
                         .title("Task Manager API")
                         .description("Spring Boot REST API with JWT Authentication")
